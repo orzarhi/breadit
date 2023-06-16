5aba94283c207cf9e8ab1cfc638d7b3021e8d106
Maxsoft Ltd <95671078+MaxsoftLtd@users.noreply.github.com>
2023-06-16 16:11:58 +0300

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+import { Icons } from "./Icons"
+import { buttonVariants } from "./ui/Button"
+import { getAuthSession } from "@/lib/auth"
+import { UserAccountNav } from "./UserAccountNav"
+
+export const Navbar = async () => {
+
+    const session = await getAuthSession()
+
+    return (
+        <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
+            <div className="container flex items-center justify-between h-full gap-2 mx-auto max-w-7xl">
+                <Icons.logo className="w-8 h-8 sm:h-6 sm:w-6" />
+                <Link href="/" className="flex items-center gap-2">
+                    <p className="hidden text-sm font-medium text-zinc-700 md:block">Breadit</p>
+                </Link>
+
+                {/* Serach bar */}
+
+                {session?.user ?
+                    <UserAccountNav user={session.user} />
+                    :
+                    <Link href="sign-in" className={buttonVariants()}>Sign in</Link>
+                }
+            </div>
+        </div>
+    )
+}
