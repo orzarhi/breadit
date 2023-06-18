36b86aa526da7a072ba35d5afada7d9c4693780f
Maxsoft Ltd <95671078+MaxsoftLtd@users.noreply.github.com>
2023-06-18 13:26:29 +0300

    Added create new subreddits and leave from subreddits

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -3,16 +3,19 @@
 
 import { Button } from "@/components/ui/Button"
 import { Input } from "@/components/ui/Input"
+import { useCustomToast } from "@/hooks/use-custom-toast"
+import { toast } from "@/hooks/use-toast"
 import { CreateSubredditPayload } from "@/lib/validators/subreddit"
 import { useMutation } from "@tanstack/react-query"
-import axios from "axios"
+import axios, { AxiosError } from "axios"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 const page = () => {
     const [input, setInput] = useState<string>("")
-
     const router = useRouter()
+    const { loginToast } = useCustomToast()
+
 
     const { mutate: createCommunity, isLoading } = useMutation({
         mutationFn: async () => {
@@ -21,6 +24,35 @@ const page = () => {
             const { data } = await axios.post('/api/subreddit', payload)
             return data as string;
         },
+        onError: (err) => {
+            if (err instanceof AxiosError) {
+                if (err.response?.status === 409) {
+                    return toast({
+                        title: 'Subreddit already exists.',
+                        description: 'Please choose a different subreddit name.',
+                        variant: 'destructive'
+                    })
+                }
+                if (err.response?.status === 422) {
+                    return toast({
+                        title: 'Invalid subreddit name.',
+                        description: 'Please choose a name between 3 and 21 characters.',
+                        variant: 'destructive'
+                    })
+                }
+                if (err.response?.status === 401) {
+                    return loginToast();
+                }
+            }
+            toast({
+                title: 'There was an error.',
+                description: 'Could not create subreddit.',
+                variant: 'destructive'
+            })
+        },
+        onSuccess: (data) => {
+            router.push(`/r/${data}`)
+        }
     })
 
     return (
@@ -46,8 +78,9 @@ const page = () => {
                     <Button
                         isLoading={isLoading}
                         disabled={input.length === 0}
-                        onClick={() => createCommunity()}
-                    >Create Community</Button>
+                        onClick={() => createCommunity()}>
+                        Create Community
+                    </Button>
                 </div>
             </div>
         </main >
